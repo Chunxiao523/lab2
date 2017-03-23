7f39fcc1e11041ed4c727948786644f744a7b759
Chunxiao523 <cz31@rice.edu>
2017-03-22 20:05:47 -0500

    makefile

--- idle.c
+++ idle.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <comp421/hardware.h>
 
-int main()
-{
+int
+main() {
+	printf("Idle Process Initialized.\n");
 	while(1) {
 		Pause();
-		 printf("idle process running \n");
 	}
-    return 0;
 }
