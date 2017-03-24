160e84ed147b5857c3e58ac4d946e0fcccd6be87
Chunxiao523 <cz31@rice.edu>
2017-03-24 16:48:22 -0500

    123

--- load.c
+++ load.c
@@ -200,13 +200,16 @@ LoadProgram(char *name, char **args, ExceptionInfo *info)
 
     for (; i < text_npg + data_bss_npg + MEM_INVALID_PAGES; i ++) {
         if (i < text_npg + MEM_INVALID_PAGES) {
+            TracePrintf(0, "LoadProgram: text page %d \n", i);
             process_page_table[i].pfn = find_free_page();
             TracePrintf(0, "LoadProgram: find free page  \n");
             process_page_table[i].valid = 1;
             process_page_table[i].kprot = PROT_READ|PROT_WRITE;
             process_page_table[i].uprot = PROT_READ|PROT_EXEC;
         } else {
+            TracePrintf(0, "LoadProgram: data page %d \n", i);
             process_page_table[i].pfn = find_free_page();
+            TracePrintf(0, "LoadProgram: find free page  \n");
             process_page_table[i].valid = 1;
             process_page_table[i].kprot = PROT_READ|PROT_WRITE;
             process_page_table[i].uprot = PROT_READ|PROT_WRITE;
