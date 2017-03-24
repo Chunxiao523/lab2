fcb4b6b08e546691f683e84e22c34e118c0d3f5b
Chunxiao523 <cz31@rice.edu>
2017-03-24 15:39:04 -0500

    123

--- yalnix.c
+++ yalnix.c
@@ -185,7 +185,7 @@ void KernelStart(ExceptionInfo *info, unsigned int pmem_size, void *orig_brk, ch
 }
 
 int SetKernelBrk(void *addr) {
-	printf("1232132132");
+	TracePrintf(2, "Setting kernel brk.\n");
 	if (*(unsigned long *)addr >= VMEM_1_LIMIT || *(unsigned long *)addr < VMEM_1_BASE) return -1;
 	if (vir_mem == 0) {
 		kernel_cur_break = addr;
