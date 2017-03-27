65b6315a85aa15209f0db56337a882cd3959a3a7
Chunxiao523 <cz31@rice.edu>
2017-03-27 11:12:54 -0500

    last try to deal with flush problem. Or go to sleep...

--- yalnix.c
+++ yalnix.c
@@ -405,9 +405,9 @@ SavedContext *MyKernelSwitchFunc(SavedContext *ctxp, void *p1, void *p2) {
        }
 
     }
-
+    p2_pt[508].valid = 1;
     WriteRegister(REG_PTR0, (RCS421RegVal)va2pa(p2_pt)); // Set the register for region 0
-    TracePrintf(2, "Context Switch: Set the register for region 0， %d\n", p2_pt[508].valid);
+    TracePrintf(2, "Context Switch: Set the register for region 0， %d\n", p2_pt[508].pfn);
     WriteRegister(REG_TLB_FLUSH, TLB_FLUSH_0); // flush
     TracePrintf(2, "Context Switch: finish context switch\n");
 	return &pcb_ptr2->ctx;
