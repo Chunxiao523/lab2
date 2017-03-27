89bbba589b7f824ee64354f940a3254f4aabeee4
Chunxiao523 <cz31@rice.edu>
2017-03-27 13:46:27 -0500

    solve the context switch valid problem

--- yalnix.c
+++ yalnix.c
@@ -371,6 +371,12 @@ SavedContext *MyKernelSwitchFunc(SavedContext *ctxp, void *p1, void *p2) {
 
     struct pte *p1_pt = pcb_ptr1->page_table;
     struct pte *p2_pt = pcb_ptr2->page_table;
+
+    void *ret_addr = memcpy((void *)pcb_ptr2->ctx, (void *)ctxp, sizeof(SavedContext));
+    if(ret_addr != (void *)pcb_ptr2->ctx){
+        fprintf(stderr,"switchAddressSpace: SavedContext copy failed!\n");
+        return ctxp;
+    }
     int i;
     unsigned long addr;
     TracePrintf(2, "Context Switch: Process 1 and Process 2 page table initialized, begin loop\n");
@@ -421,7 +427,9 @@ SavedContext *MyKernelSwitchFunc(SavedContext *ctxp, void *p1, void *p2) {
     TracePrintf(2, "Context Switch: Set the register for region 0， %d\n", p2_pt[508].valid);
     WriteRegister(REG_TLB_FLUSH, TLB_FLUSH_0); // flush
     TracePrintf(2, "Context Switch: finish context switch\n");
-	return ((pcb *)p2)->ctx;
+    TracePrintf(2, "Context Switch: finish context switch  %s\n", &p2 -> ctx);
+    TracePrintf(2, "Context Switch: finish context switch  %s\n", &pcb_ptr2 -> ctx);
+	return &pcb_ptr2->ctx;
 }
 /**
  * Function to map virtual address to physical address
