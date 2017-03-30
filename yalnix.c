b6e3256f0adaa88fff379fcc3b20deb62ada4621
Chunxiao523 <cz31@rice.edu>
2017-03-29 22:45:05 -0500

    testing delay and debugging...

--- yalnix.c
+++ yalnix.c
@@ -268,11 +268,11 @@ int SetKernelBrk(void *addr) {
 		if(addr > kernel_cur_break) {
 			TracePrintf(2, "Set kernel brk: addr > kernel_cur_break \n");
 			int i;
-            TracePrintf(2, "Set kernel brk: need %d, have %d \n", DOWN_TO_PAGE(*(unsigned long *)addr) - UP_TO_PAGE(kernel_cur_break),PAGESIZE*free_page_num);
-            if ( DOWN_TO_PAGE(*(unsigned long *)addr) - UP_TO_PAGE(kernel_cur_break) > PAGESIZE*free_page_num) {
-                TracePrintf(2, "Set Kernel brk: Not enough pages\n");
-                return -1;
-            }
+//            TracePrintf(2, "Set kernel brk: need %d, have %d \n", DOWN_TO_PAGE(*(unsigned long *)addr) - UP_TO_PAGE(kernel_cur_break),PAGESIZE*free_page_num);
+//            if ( DOWN_TO_PAGE(*(unsigned long *)addr) - UP_TO_PAGE(kernel_cur_break) > PAGESIZE*free_page_num) {
+//                TracePrintf(2, "Set Kernel brk: Not enough pages\n");
+//                return -1;
+//            }
             TracePrintf(2, "Set Kernel brk: working now!...\n");
 			/* Given a virtual page number, assign a physical page to its corresponding pte entry */
 			for(i = (UP_TO_PAGE(kernel_cur_break) - VMEM_1_BASE)>>PAGESHIFT; i < (UP_TO_PAGE(addr) - VMEM_1_BASE)>>PAGESHIFT; i++) {
