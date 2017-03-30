eb288b8df3579af3c5a58cc2953360a242586899
Chunxiao523 <cz31@rice.edu>
2017-03-30 12:37:48 -0500

    add delaypre field to pcb struct

--- yalnix.c
+++ yalnix.c
@@ -257,10 +257,11 @@ void KernelStart(ExceptionInfo *info, unsigned int pmem_size, void *orig_brk, ch
     } else {
         TracePrintf(2, "Kernel Start:Load process from argument.\n");
         LoadProgram(cmd_args[0],cmd_args,info, process_page_table);
+        TracePrintf(2, "Kernel Start:Load process back.\n");
         cur_Proc->page_table = process_page_table;
         cur_Proc->pid = pid++;
         cur_Proc->ctx = (SavedContext *)malloc(sizeof(SavedContext));
-        TracePrintf(2, "Kernel Start:Load process from argument finished.\n");
+        TracePrintf(2, "Kernel Start: Load process from argument finished.\n");
     }
 
 }
