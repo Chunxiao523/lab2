e1c247db5ad1b2e91d80bf4afef081b65bed8655
Chunxiao523 <cz31@rice.edu>
2017-03-30 11:54:59 -0500

    add delaypre field to pcb struct

--- delaytest.c
+++ delaytest.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 #include <comp421/yalnix.h>
 #include <comp421/hardware.h>
-   
-int main(int argc, char **argv)
+
+int
+main(int argc, char **argv)
 {
     fprintf(stderr, "Starting delay...\n");
-    Delay(5);
+    Delay(atoi(argv[1]));
     fprintf(stderr, "Delay finished!\n");
 
     Exit(0);
