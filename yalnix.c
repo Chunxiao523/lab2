a40a7eb19c3e87fdc1b02293be11cc91d19661cf
Chunxiao523 <cz31@rice.edu>
2017-03-30 21:19:22 -0500

    add delaypre field to pcb struct

--- yalnix.c
+++ yalnix.c
@@ -250,7 +250,7 @@ void KernelStart(ExceptionInfo *info, unsigned int pmem_size, void *orig_brk, ch
     vir_mem = 1;
     TracePrintf(2, "Kernel Start: virtual memory enabled.\n");
    //if(cmd_args[0] == NULL) {
-        /*
+    /*
 	 * Create idle and init process
 	 */
         idle = (pcb*)malloc(sizeof(pcb));
@@ -277,7 +277,7 @@ void KernelStart(ExceptionInfo *info, unsigned int pmem_size, void *orig_brk, ch
             LoadProgram("idle",cmd_args, info, process_page_table);
         else if(cur_Proc->pid==1) {
             if (cmd_args==NULL || cmd_args[0]==NULL) LoadProgram("init",cmd_args,info, init_page_table);
-            else LoadProgram(cmd_args[0],cmd_args, info, process_page_table);
+            else LoadProgram(cmd_args[0], cmd_args, info, process_page_table);
         }
 }
 /**
